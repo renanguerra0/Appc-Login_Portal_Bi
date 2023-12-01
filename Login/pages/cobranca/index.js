1966979682f88ef85c85a9f4168de101819bab4f
Renan Guerra <renanguerradev@gmail.com>
2023-12-01 08:24:13 -0300

    Ajuste v18

--- Login/pages/cobranca/index.js
+++ Login/pages/cobranca/index.js
@@ -76,20 +76,3 @@ function showMessage(){
 function hideMessage(){
   mensagem.style.display = "none"; 
 }
-
-
-
-/*Loop para buscar as imagens na pasta*/
-/*
-for(var i = 0; i < 10; i++){    
-    var img = document.createElement("img"); //Criar elemento img
-    img.src = "../../10. Dataflow/1. Departamentos/RecursosHumanos/Comunicados/" + i + ".png"; //Atribuindo a propriedade source da imagem  
-    img.width = "400";
-
-    img.onerror = function() {
-        console.log("Erro: A imagem " + this.src + " não foi encontrada."); //Verificando
-        this.style.display = "none"; //Caso não encontre a imagem, ocultar o erro. 
-    };
-        document.getElementById("content").appendChild(img); //Adicionando imagem como filha de content e exibindo na tela
-}
-*/
